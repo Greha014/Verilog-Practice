b3391b18ecf56b05926505e910c7ec68bebc7986
Greha014 <allegrehareddy@outlook.com>
2026-06-08 22:31:44 +0530

    Add files via upload

---
+++ 01_Basic_Gates/and_gate.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.05.2026 23:47:12
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(
+    input a,
+    input b,
+    output y
+    );
+    assign y = a&b;
+endmodule
